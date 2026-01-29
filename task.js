e355613835ef2d6dfd92f5756f7bfabf77fb075b
Arseny <arsenijstolarov378@gmail.com>
2026-01-29 21:32:38 +0500

    feat(js): условия и логические проверки для задач

--- task.js
+++ task.js
@@ -3,14 +3,15 @@ let title = ""
 if(title === ""){
   console.log("Название задачи не указано");
 }else{
-  console.log("Задача:", title);
+  console.log("Задача", title);
 }
 
 
-let tasks = 5
+let tasks = 5;
+
 if(tasks === 0){
   console.log("Список пуст");
-}else if(tasks <=3){
+}else if(tasks <= 3){
   console.log("Немного задач");
 }else{
   console.log("Много задач");
